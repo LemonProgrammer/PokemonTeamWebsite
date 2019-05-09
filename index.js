c8d76e8cd77e85651b5d6624157a375074ffde09
Tipping Bottles <haileyroberts23331@gmail.com>
2019-05-09 08:58:47 -0600

    merge

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+let data;
+
+let pokemon = Math.floor(Math.random()*800);
+let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    let new_url = data.name;
+    console.log(new_url);
+    fetch(new_url)
+      .then(response1 => response1.json())
+     .then(data1 => {
+       console.log(data1);
+     });
+  })
+  .catch(e => console.log(e));
